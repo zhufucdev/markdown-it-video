10c5f16ade8d4bf62c4b15a305c9e32792165d04
Brian J. Geiger <bgeiger@pobox.com>
2017-10-17 15:41:21 -0400

    Return in the default case to fix eslint

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ function video_url(service, videoID, options) {
       'landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5N1lQVHkxSHFxazZ0UUNCRHloSXZROHh3PT0&amp;' +
       'landing_sign=1kD6c0N6aYpMUS0wxnQjxzSqZlEB8qNFdxtdjYhwSuI';
   }
+  return service;
 }
 
 function tokenize_video(md, options) {
