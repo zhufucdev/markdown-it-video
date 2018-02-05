74cab54d8d326823b2758853986ce844956ddd08
John Tordoff <john@cos.io>
2018-02-05 13:58:40 -0500

    change service name to file

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ function videoUrl(service, videoID, options) {
         '/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;' +
         'landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5N1lQVHkxSHFxazZ0UUNCRHloSXZROHh3PT0&amp;' +
         'landing_sign=1kD6c0N6aYpMUS0wxnQjxzSqZlEB8qNFdxtdjYhwSuI';
-    case 'mfr':
+    case 'file':
       return "https://mfr.osf.io/render?url=https://osf.io/" + videoID + "/?action=download"
     default:
       return service;
